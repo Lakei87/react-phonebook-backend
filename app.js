2ae8ed4f517d02498451a5cd296073851df5ebe9
Lakei87 <monastyrskyis@gmail.com>
2023-05-09 20:18:05 +0200

    create routes

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
 
+const { authRouter, contactsRouter } = require('./routes/api');
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
+app.use("/api/contacts", contactsRouter);
+
+app.use((_, res) => {
+    res.status(404).json({ message: "Not found" });
+});
 
 module.exports = app;
---
+++ routes/api/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/signup');
+router.post('/login');
+router.post('/logout');
+router.get('/current');
+
+module.exports = router;
