44e5fe805ff0f886d27117fcf5b6b7b95332d2f1
Lakei87 <monastyrskyis@gmail.com>
2023-05-16 11:21:07 +0200

    create authenticate middleware

---
+++ middlewares/authenticate.js
@@ -0,0 +1,30 @@
+const { User } = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const createError = require('http-errors');
+const { SECRET_KEY } = process.env;
+
+const authenticate = async (req, _, next) => {
+    const { authorization = "" } = req.headers;
+    const [bearer, token] = authorization.split(" ");
+    
+    if (bearer !== "Bearer") {
+        throw createError(401, "Not authorized");
+    }
+
+    try {
+        const { id } = jwt.verify(token, SECRET_KEY);
+        const user = await User.findById(id);
+
+        if (!user || !user.token) {
+            throw createError(401, "Not authorized");
+        };
+
+        req.user = user;
+        next();
+    } catch (error) {
+        next(createError(401, "Not authorized"));
+    }
+};
+
+module.exports = authenticate;
