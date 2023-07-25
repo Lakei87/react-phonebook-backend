2c6275bead0bf4507e1dfbae9451dbe70914c517
Lakei87 <monastyrskyis@gmail.com>
2023-07-25 22:29:49 +0200

    bug fix when deleting a contact

--- controllers/contacts/remove.js
+++ controllers/contacts/remove.js
@@ -4,14 +4,15 @@ const createError = require('http-errors');
 
 const remove = async (req, res) => {
     const { id } = req.params;
-    const result = await Contact.findByIdAndRemove(id);
+    const deletedContact = await Contact.findByIdAndRemove(id);
 
-    if (!result) {
+    if (!deletedContact) {
         throw createError(404, `Was not found contact with id: ${id}`);
     };
 
     res.json({
-        message: "Delete success"
+        message: "Delete success",
+        deletedContact,
     });
 };
 
